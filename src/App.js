b406c6bfa6fa9dee90d4c9369460442d84567163
cat_young <cyoun002@gmail.com>
2025-03-25 15:45:55 -0400

    Render quote cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Quotes from "./components/quotes/Quotes";
 import { Loader } from "react-feather";
 import "./App.css";
 
@@ -10,7 +11,7 @@ function App() {
   const quotesUrl = 
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
 
-  const fetchQuotes = async() => {
+  const fetchQuotes = async () => {
     try{
       setLoading(true);
       const response = await fetch(quotesUrl);
@@ -29,7 +30,7 @@ function App() {
   return (
     <div className='App'>
       <Header />
-      <main>{loading ? <Loader /> : JSON.stringify(quotes)}</main>
+      <main>{loading ? <Loader /> : <Quotes quotes={quotes} />}</main>
       <Footer />
     </div>
   );
