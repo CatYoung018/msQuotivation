3104079b561f31314837cf78560c1bec947c1e01
cat_young <cyoun002@gmail.com>
2025-03-27 07:41:58 -0400

    Filter quotes by category

--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -1,16 +1,22 @@
 import React from "react";
 import QuoteCard from "./QuoteCard";
+import CategoryForm from "./CategoryForm";
 
-const Quotes = ({quotes}) => {
-    return(
-        <section className="all-quotes">
-            <div className="quotes wrapper">
-                {quotes.map((quote) => (
-                    <QuoteCard key={quote.id} quote={quote} />
-                ))
-                }</div>
-        </section>
-    );
-};
+function Quotes({ filteredQuotes, category, categories, handleCategoryChange }) {
+  return (
+    <section className='all-quotes'>
+      <div className='quotes wrapper'>
+        <div className='category-header'>
+          <p>Browse through your collection of quotes.</p>
+          <CategoryForm categories={categories} category={category} handleCategoryChange={handleCategoryChange} />
+        </div>
+
+        {filteredQuotes.map((quote) => (
+          <QuoteCard key={quote.id} quote={quote} />
+        ))}
+      </div>
+    </section>
+  );
+}
 
 export default Quotes;
